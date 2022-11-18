25f87677086a684fb07da42ab9fa633d7d4ec847
narendram224 <narendram224.nm@gmail.com>
2022-11-18 16:07:37 +0530

    Minor changes

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -3,7 +3,7 @@ import styles from './MainLayout.module.scss';
 const MainLayout = () => {
   return (
     <div id="main_layout" className={styles.mainLayoutContainer}>
-      MainLayou
+      MainLayout
     </div>
   );
 };
